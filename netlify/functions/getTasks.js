0c65d1a2700574f9aa51ca37770325f868812cf9
Bhavani1302 <Bhavisha@2022>
2025-04-17 12:37:31 +0530

    Move db.json to functions directory and fix path for deployment

--- netlify/functions/getTasks.js
+++ netlify/functions/getTasks.js
@@ -2,7 +2,7 @@ const fs = require("fs");
 const path = require("path");
 
 exports.handler = async () => {
-  const filePath = path.resolve(__dirname, "../../db.json");
+  const filePath = path.resolve(__dirname, "db.json");
   try {
     const data = fs.readFileSync(filePath, "utf8");
     const json = JSON.parse(data);
